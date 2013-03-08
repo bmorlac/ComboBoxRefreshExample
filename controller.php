b7bbb965a8deee78a1960db98802f82195fd3ea8
bmorlac <benjamin.morlac@N-01-109>
2013-03-08 17:07:10 +0100

    version 3 adds a content negotiation example

--- controller.php
+++ controller.php
@@ -1,15 +1,21 @@
 <?php
 // print_r($_POST);
 
-$actress = $_POST['actress'];
-$data = array();
-switch ($actress) {
-  case "Megan Fox": $data = array("Transformers", "Transformers - Die Rache", "Jennifer's Body"); break;
-  case "Jessica Alba": $data = array("Sin City", "Machete", "Die Killerhand", "The Eye"); break;
-  case "Kate Beckinsale": $data = array("Underworld", "Aviator"); break;
-  case "Milla Jovovich": $data = array("Resident Evil", "Das f&uuml;nfte Element", "Ultraviolet"); break;
-  case "Olivia Wilde": $data = array("Cowboys & Aliens", "In Time"); break;
+if ((strpos(strtolower($_SERVER['HTTP_ACCEPT']), "application/json") !== false)) {
+  $actress = $_POST['actress'];
+  $data = array();
+  switch ($actress) {
+    case "Megan Fox": $data = array("Transformers", "Transformers - Die Rache", "Jennifer's Body"); break;
+    case "Jessica Alba": $data = array("Sin City", "Machete", "Die Killerhand", "The Eye"); break;
+    case "Kate Beckinsale": $data = array("Underworld", "Aviator"); break;
+    case "Milla Jovovich": $data = array("Resident Evil", "Das f&uuml;nfte Element", "Ultraviolet"); break;
+    case "Olivia Wilde": $data = array("Cowboys & Aliens", "In Time"); break;
+  }
+  header("Content-Type: application/json");
+  echo json_encode(array("movies" => $data));
 }
-echo json_encode(array("movies" => $data));
+else {
+  echo "This website only returns JSON responses.";
+}
 
 ?>
