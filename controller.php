45c2c79ba8604576a6744d86ce9caf57d8e1b2eb
bmorlac <benjamin.morlac@N-01-109>
2013-03-08 15:37:33 +0100

    version 2 now returns multiple movie entries in a JSON encoded response

--- controller.php
+++ controller.php
@@ -2,20 +2,14 @@
 // print_r($_POST);
 
 $actress = $_POST['actress'];
-if ($actress == "Megan Fox") {
- echo "Transformers";
+$data = array();
+switch ($actress) {
+  case "Megan Fox": $data = array("Transformers", "Transformers - Die Rache", "Jennifer's Body"); break;
+  case "Jessica Alba": $data = array("Sin City", "Machete", "Die Killerhand", "The Eye"); break;
+  case "Kate Beckinsale": $data = array("Underworld", "Aviator"); break;
+  case "Milla Jovovich": $data = array("Resident Evil", "Das f&uuml;nfte Element", "Ultraviolet"); break;
+  case "Olivia Wilde": $data = array("Cowboys & Aliens", "In Time"); break;
 }
-elseif ($actress == "Jessica Alba") {
- echo "Sin City";
-}
-elseif ($actress == "Kate Beckinsale") {
- echo "Underworld";
-}
-elseif ($actress == "Milla Jovovich") {
- echo "Resident Evil";
-}
-elseif ($actress == "Olivia Wilde") {
- echo "Cowboys & Aliens";
-}
+echo json_encode(array("movies" => $data));
 
 ?>
